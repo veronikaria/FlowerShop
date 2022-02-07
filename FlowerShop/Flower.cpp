813f85071347b7020b7a748f6b7eaa731439181d
Veronika <veronika.ria.v@gmail.com>
2022-02-08 01:47:44 +0200

    getter-setter for class flower

--- FlowerShop/Flower.cpp
+++ FlowerShop/Flower.cpp
@@ -24,3 +24,65 @@ Flower::Flower(int height, int size, string color, double price, int quantity, d
 Flower::~Flower() { }
 
 
+int Flower::getHeight()
+{
+	return this->height;
+}
+
+void Flower::setHeight(int height)
+{
+	this->height = height;
+}
+
+
+int Flower::getSize()
+{
+	return this->size;
+}
+
+void Flower::setSize(int size)
+{
+	this->size = size;
+}
+
+string Flower::getColor()
+{
+	return this->color;
+}
+
+void Flower::setColor(string color)
+{
+	this->color = color;
+}
+
+double Flower::getPrice()
+{
+	return this->price;
+}
+
+void Flower::setPrice(double price)
+{
+	this->price = price;
+}
+
+
+int Flower::getQuantity()
+{
+	return this->quantity;
+}
+
+void Flower::setQuantity(int quantity)
+{
+	this->quantity = quantity;
+}
+
+double Flower::getDeliveryRate()
+{
+	return this->deliveryRate;
+}
+
+void Flower::setDeliveryRate(double deliveryRate)
+{
+	this->deliveryRate = deliveryRate;
+}
+
